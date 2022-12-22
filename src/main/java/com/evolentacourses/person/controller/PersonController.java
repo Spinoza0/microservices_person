86278dd7399a60e913a5d05895a410287fa5166c
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2022-12-22 16:35:29 +0300

    using Eureka

--- src/main/java/com/evolentacourses/person/controller/PersonController.java
+++ src/main/java/com/evolentacourses/person/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.evolentacourses.person.model.Person;
 import com.evolentacourses.person.model.Weather;
 import com.evolentacourses.person.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,8 @@ public class PersonController {
     private PersonRepository repository;
     @Autowired
     private RestTemplate restTemplate;
+    @Value("${location.url}")
+    private String locationUrl;
 
     @GetMapping
     public Iterable<Person> findAll() {
@@ -34,9 +37,8 @@ public class PersonController {
     public ResponseEntity<Weather> getWeather(@PathVariable int id) {
         return repository.findById(id)
                 .map(person -> {
-                    String location = person.getLocation();
-                    Weather weather = restTemplate
-                            .getForObject("http://localhost:8083/weather?location=" + location, Weather.class);
+                    String url = String.format("%s=%s", locationUrl, person.getLocation());
+                    Weather weather = restTemplate.getForObject(url, Weather.class);
                     return new ResponseEntity<>(weather, HttpStatus.OK);
                 })
                 .orElse(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
@@ -44,8 +46,8 @@ public class PersonController {
 
     @PostMapping
     public ResponseEntity<Person> save(@RequestBody Person person) {
-        return repository.findById(person.getId()).isPresent()
-                ? new ResponseEntity<>(repository.findById(person.getId()).get(), HttpStatus.BAD_REQUEST)
-                : new ResponseEntity<>(repository.save(person), HttpStatus.CREATED);
+        return repository.findById(person.getId())
+                .map(it -> new ResponseEntity<>(it, HttpStatus.BAD_REQUEST))
+                .orElse(new ResponseEntity<>(repository.save(person), HttpStatus.CREATED));
     }
 }
